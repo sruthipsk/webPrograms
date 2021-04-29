455d8782ccf10ea4f6f1a267609ccd29d8800a18
sruthipsk <79830818+sruthipsk@users.noreply.github.com>
2021-04-29 21:19:19 +0530

    Create strcmp.php

---
+++ progrm_17/strcmp.php
@@ -0,0 +1,15 @@
+<html>
+<head> <title> String Function:strcmp() </title> </head>
+<body>
+ <?php
+$str1 = 'a';
+$str2 = 'b';
+echo strcmp($str1, $str2)."<br/>";
+$str3 = 'b';
+$str4 = 'a';
+echo strcmp($str3, $str4)."<br/>";
+$str5 = "Anil";
+$str6 = "anil";
+echo strcmp($str5, $str6)."<br/>";
+ ?>
+</body></html>
