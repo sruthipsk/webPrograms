a3fd4fcb00a3c512be3f642d82ac38e0cbd59350
sruthipsk <sruthiskappummal@gmail.com>
2021-04-30 12:39:18 +0530

    commiting

---
+++ swapping.php
@@ -0,0 +1,12 @@
+<?php  
+$a = 20;  
+$b = 40; 
+echo "before swapping:<br><br>";
+echo "a =".$a."<br> b=".$b;
+$third = $a;  
+$a = $b;  
+$b = $third;  
+echo "<br>After swapping:<br><br>";  
+echo "a =".$a." <br> b=".$b;  
+?>  
+
